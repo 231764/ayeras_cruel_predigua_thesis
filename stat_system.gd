e44452ca8e3b510e2fa6d63441dd8cda378cc860
Enrico Cruel <pio.cruel@student.ateneo.edu>
2026-08-28 23:14:27 +0800

    Added interface to check overall player stats and script for future functions and stat-handling for gameplay.

---
+++ stat_system.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var stat_interface: CanvasLayer
+@export var happy_bar: ProgressBar
+@export var social_bar: ProgressBar
+@export var current_balance: Label
+@export var savings: Label
+@export var credit_balance: Label
+@export var credit_score: Label
+@export var debt_balance: Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
